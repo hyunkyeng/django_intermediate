4dfc79fa006c54adffbed2ba107bb3f3ca880f1b
hyunkyeng <hyunk4593@gmail.com>
2019-03-14 01:40:34 +0000

    00_RESTful URL setting

--- boards/urls.py
+++ boards/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'boards'
 
 urlpatterns = [
-    path('<int:pk>/update/', views.update, name='update'),
+    # path('<int:pk>/update/', views.update, name='update'),  # GET(EDIT) / POST(UPDATE)
     path('<int:pk>/edit/', views.edit, name='edit'),
-    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/delete/', views.delete, name='delete'),  # POST로했을 때 delete가 되도록
     path('<int:pk>/', views.detail, name='detail'),
-    path('create/', views.create, name='create'),
-    path('new/', views.new, name='new'),
+    # path('create/', views.create, name='create'),
+    path('new/', views.new, name='new'),    # GET으로 들어로면 (NEW) / POST로 들어오면 (CREATE)
     path('', views.index, name='index'),    
 ]
